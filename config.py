7698895ea2ed315fdc08e3e8e2e5cfebac123a75
ㅤ𝛎 𝜄 ᥉ ᥴ᥆ ᥉ 𝜄 𝗍 𝛄 ✞ㅤ <166417113+viscity@users.noreply.github.com>
2026-01-29 11:32:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -89,23 +89,23 @@ autoclean = []
 confirmer = {}
 file_cache: dict[str, float] = {}
 
-START_IMG_URL = ["https://te.legra.ph/file/5fd13f2cc0d03bce9f7f2.jpg",
-                 "https://te.legra.ph/file/c15d01b3e6b40ea141dc9.jpg",
-                 "https://te.legra.ph/file/5fd13f2cc0d03bce9f7f2.jpg"]
+START_IMG_URL = ["https://files.catbox.moe/p7p32v.jpg",
+                 "https://files.catbox.moe/vfgznw.jpg",
+                 "https://files.catbox.moe/fzl3n3.jpg"]
     
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://telegra.ph/file/87f680aead03443f291b0.jpg"
+    "PING_IMG_URL", "https://files.catbox.moe/bs8kwb.jpg"
 )
-PLAYLIST_IMG_URL = "https://graph.org/file/c95a687e777b55be1c792.jpg"
-STATS_IMG_URL = "https://telegra.ph/file/edd388a42dd2c499fd868.jpg"
-TELEGRAM_AUDIO_URL = "https://telegra.ph/file/492a3bb2e880d19750b79.jpg"
-TELEGRAM_VIDEO_URL = "https://telegra.ph/file/492a3bb2e880d19750b79.jpg"
-STREAM_IMG_URL = "https://graph.org/file/ff2af8d4d10afa1baf49e.jpg"
-SOUNCLOUD_IMG_URL = "https://graph.org/file/c95a687e777b55be1c792.jpg"
-YOUTUBE_IMG_URL = "https://graph.org/file/e8730fdece86a1166f608.jpg"
-SPOTIFY_ARTIST_IMG_URL = "https://graph.org/file/0bb6f36796d496b4254ff.jpg"
-SPOTIFY_ALBUM_IMG_URL = "https://graph.org/file/0bb6f36796d496b4254ff.jpg"
-SPOTIFY_PLAYLIST_IMG_URL = "https://graph.org/file/0bb6f36796d496b4254ff.jpg"
+PLAYLIST_IMG_URL = "https://files.catbox.moe/vfgznw.jpg"
+STATS_IMG_URL = "https://files.catbox.moe/fzl3n3.jpg"
+TELEGRAM_AUDIO_URL = "https://files.catbox.moe/p7p32v.jpg"
+TELEGRAM_VIDEO_URL = "https://files.catbox.moe/fzl3n3.jpg"
+STREAM_IMG_URL = "https://files.catbox.moe/vfgznw.jpg"
+SOUNCLOUD_IMG_URL = "https://files.catbox.moe/p7p32v.jpg"
+YOUTUBE_IMG_URL = "https://files.catbox.moe/p7p32v.jpg"
+SPOTIFY_ARTIST_IMG_URL = "https://files.catbox.moe/vfgznw.jpg"
+SPOTIFY_ALBUM_IMG_URL = "https://files.catbox.moe/fzl3n3.jpg"
+SPOTIFY_PLAYLIST_IMG_URL = "https://files.catbox.moe/p7p32v.jpg"
 
 
 
